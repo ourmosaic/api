8e3e6e252e980f49022b8bd1dd67f6faad705e3b
florentin <florentinmartorell@gmail.com>
2026-03-14 21:04:13 +0100

    neutral version for the moment

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Version } from '@nestjs/common';
+import { Controller, Get, Version, VERSION_NEUTRAL } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  @Version('1')
+  @Version(VERSION_NEUTRAL)
   getHello(): { message: string; version: string } {
     return this.appService.getHello();
   }
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post, Req, UseGuards, Version } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, UseGuards, Version, VERSION_NEUTRAL } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
 import type { AuthenticationResponseDto } from './dto/authentication.result.dto';
@@ -16,25 +16,25 @@ export class AuthController {
         private readonly authService: AuthService
     ) {}
 
-    @Version('1')
+    @Version(VERSION_NEUTRAL)
     @Post('register')
     async register(@Body() registerDto: RegisterDto): Promise<AuthenticationResponseDto> {
         return this.authService.registerUser(registerDto);
     }
 
-    @Version('1')
+    @Version(VERSION_NEUTRAL)
     @Post('login')
     async login(@Body() loginDto: LoginDto): Promise<AuthenticationResponseDto> {
         return this.authService.login(loginDto);
     }
 
-    @Version('1')
+    @Version(VERSION_NEUTRAL)
     @Post('token/refresh')
     async refreshTokens(@Body() refreshTokenDto: RefreshTokenDto): Promise<AuthenticationResponseDto> {
         return this.authService.refreshTokens(refreshTokenDto.refreshToken);
     }
 
-    @Version('1')
+    @Version(VERSION_NEUTRAL)
     @Get('me')
     @UseGuards(AuthGuard)
     async getMe(@Req() req: RequestWithUser) {
