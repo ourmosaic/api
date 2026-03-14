6f8afe029f25d0925632ed49b4a5cffa597b3f66
florentin <florentinmartorell@gmail.com>
2026-03-14 19:55:04 +0100

    Fix database connection

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,10 +1,18 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client/extension';
+import { ConfigService } from '@nestjs/config';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
-    constructor() {
+    constructor(private readonly configService: ConfigService) {
+        const databaseUrl = configService.get<string>('DATABASE_URL');
+        if (!databaseUrl) throw new Error('DATABASE_URL is not defined in environment variables');
+        const adapter = new PrismaPg({
+            connectionString: databaseUrl,
+        })
         super({
+            adapter,
             log: ['query', 'info', 'warn', 'error']
         })
     }
