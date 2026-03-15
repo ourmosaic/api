f43bd845c319d3385f6b89a4c76e8e444b74a512
florentin <florentinmartorell@gmail.com>
2026-03-15 17:43:13 +0100

    prepare user module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
+  imports: [PrismaModule, AuthModule],
   providers: [UsersService],
   controllers: [UsersController]
 })
