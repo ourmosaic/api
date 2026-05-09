7214721714c6dbae529784dfd5a976427e257549
chmod 777 <florentin.martorell@etu.univ-grenoble-alpes.fr>
2026-05-09 16:17:33 +0200

    fix safety module

--- src/safety/safety.module.ts
+++ src/safety/safety.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { SafetyController } from './safety.controller';
 import { PrismaModule } from '../prisma/prisma.module';
+import { AuthModule } from '../auth/auth.module';
+import { SafetyService } from './safety.service';
 
 @Module({
   controllers: [SafetyController],
-  imports: [PrismaModule],
+  imports: [PrismaModule, AuthModule],
+  providers: [SafetyService],
 })
 export class SafetyModule {}
